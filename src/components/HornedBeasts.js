a88c37b4950e061dd791698ede6fb3671150d3b3
Saif-K-Saeed <saif.k.saeed@gmail.com>
2021-09-13 22:48:11 +0300

    lab-02

--- src/components/HornedBeasts.js
+++ src/components/HornedBeasts.js
@@ -1,12 +1,37 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import './horns.css';
 
 class HornedBeasts extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            numberOfHorns: 0
+        }
+    }
+    inceaseNumberOfHorns = () => {
+        this.setState({
+            numberOfHorns: this.state.numberOfHorns + 1
+        })
+    }
     render() {
         return (
-            <div>
-                <h2>{this.props.title}</h2>
-                <img src={this.props.image_url} alt={this.props.keyword} title={this.props.horns} />
-                <p>{this.props.description}</p>
+            <div id='cartDiv'>
+
+                <Card style={{ width: '18rem', height: '28rem' }}>
+                    <Card.Img className='cartImg'  variant="top" src={this.props.image_url} alt={this.props.keyword}    />
+                    <Card.Body>
+                        <Card.Title>{this.props.title}</Card.Title>
+                        <Card.Text> {this.props.description}
+                            <br />
+                            Number Of Vote {`[${this.state.numberOfHorns}]`}
+                        </Card.Text>
+                        <Button onClick={this.inceaseNumberOfHorns} variant="primary">Vote</Button>
+                    </Card.Body>
+                </Card>
+
             </div>
         )
     }
