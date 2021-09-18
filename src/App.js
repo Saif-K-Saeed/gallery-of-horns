30f992e8696733b62ec4700a0ed570ac3f29427d
Saif-K-Saeed <saif.k.saeed@gmail.com>
2021-09-18 04:07:40 +0300

    lab-04

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Main from './components/Main';
-import data from './components/data.json'
 import SelectedBeast from './components/SelectedBeast';
+import data from './components/data.json'
+import Form from 'react-bootstrap/Form';
 
 class App extends React.Component {
   constructor(props) {
@@ -36,11 +37,42 @@ class App extends React.Component {
       show: false
     })
   }
+  filterArray = (num) => {
+    let filterarr = data.filter(element => {
+      if (num === 0) {
+        return element;
+      } else {
+        if (element.horns === num) {
+          return true;
+        }
+        else {
+          return false;
+        }
+      }
+    });
+    return filterarr;
+
+  }
+
+  filter = async (event) => {
+    event.preventDefault();
+    await this.setState({
+      numumberhoners: event.target.value,
+      all: this.filterArray(Number(event.target.value))
+    });
+  };
 
   render() {
     return (
       <>
-        <Header></Header>
+        <Header />   <Form.Select name="hor" aria-label="Default select example" onChange={this.filter}>
+          <option>Filter a  number of horns</option>
+          <option value="0">All</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+          <option value="100">100</option>
+        </Form.Select>
 
         <Main data={this.state.data}
           selectedCard={this.selectedCard}
